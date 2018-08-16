bc956a419f433dc10c18564b07b8c5ca5c61d1b2
andreaweb <deiainluv@gmail.com>
2018-08-16 08:43:33 -0300

    map is loaded, google-maps-react is used

--- src/App.js
+++ src/App.js
@@ -1,20 +1,81 @@
-import React, { Component } from 'react';
-import * as MapsAPI from './Map.js';
+import React from 'react';
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+//import * as MapsAPI from './Map.js';
 import './App.css';
 //import google from "https://maps.googleapis.com/maps/api/js?key=AIzaSyAMSLE6fujNqNvj7opx7S3URDb9z_w_HyI&callback=initMap";
 
-class App extends Component {
-  componentDidMount(){
-   console.log(MapsAPI.initMap())
-  }
+// const PLACE= {
+//   address: 'Vicente de Carvalho, Rio de Janeiro, Brazil',
+//   position: {
+//     latitude: -22.8558216,
+//     longitude: -43.315785
+//   }
+// }
 
+const APIKEY = "AIzaSyAMSLE6fujNqNvj7opx7S3URDb9z_w_HyI";
+
+//const INITIAL_ZOOM = 14;
+
+class GoogleMapsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    }
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
   render() {
+    const style = {
+      width: '50vw',
+      height: '75vh',
+      'marginLeft': 'auto',
+      'marginRight': 'auto'
+    }
     return (
-      <div className="App" id="map" style={{"height":"100vh"}}>
-        
-      </div>
+      <Map
+        xs = { 12 }
+        style = { style }
+        google = { this.props.google }
+        onClick = { this.onMapClick }
+        zoom = { 14 }
+        initialCenter = {{ lat: -22.8558216, lng: -43.315785 }}
+      >
+
+        <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Changing Colors Garage' }
+          position = {{ lat: -22.8558216, lng: -43.315785 }}
+          name = { 'Changing Colors Garage' }
+        />
+        <InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }
+        >
+        <span>Add info here</span>
+        </InfoWindow>
+      </Map>
     );
   }
 }
-
-export default App;
+export default GoogleApiWrapper({
+    apiKey: APIKEY
+})(GoogleMapsContainer)
--- src/Map.js
+++ src/Map.js
@@ -27,9 +27,15 @@
 //     body: JSON.stringify({ shelf })
 //   }).then(res => res.json())
 
+
+//Vicente de Carvalho ID d049ef5048cc95a135790671f0d40b40a50591df
+//url https://maps.google.com/?q=Vicente+de+Carvalho,+Rio+de+Janeiro+-+State+of+Rio+de+Janeiro,+Brazil&ftid=0x997cb946fdae7b:0x4a1775041d5cd0aa
+//address <span class="extended-address">Vicente de Carvalho</span>, <span class="locality">Rio de Janeiro</span> - <span class="region">State of Rio de Janeiro</span>, <span class="country-name">Brazil</span>
+//lat -22.8558216
+//long -43.315785
 export const initMap = () => {
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  const googleURL = "https://maps.googleapis.com/maps/api/place/details/json?"
+  const googleURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAMSLE6fujNqNvj7opx7S3URDb9z_w_HyI"
     +"language=en&placeid=ChIJe679Rrl8mQARqtBcHQR1F0o&key=AIzaSyAMSLE6fujNqNvj7opx7S3URDb9z_w_HyI&callback=initMap";
   fetch(proxyurl + googleURL)
   .then(res => {let sup = res.json(); console.log(res.json); return sup})
@@ -41,3 +47,18 @@ export const initMap = () => {
   //   zoom: 8
   // });
 }
+
+// export const initMap = () => {
+//   const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//   const googleURL = "https://maps.googleapis.com/maps/api/place/details/json?"
+//     +"language=en&placeid=ChIJe679Rrl8mQARqtBcHQR1F0o&key=AIzaSyAMSLE6fujNqNvj7opx7S3URDb9z_w_HyI&callback=initMap";
+//   fetch(proxyurl + googleURL)
+//   .then(res => {let sup = res.json(); console.log(res.json); return sup})
+//   .then(data => {let mapData = data; console.log(data); return mapData})
+//   .catch(error => console.log(error))
+
+  // const map = new google.maps.Map(document.getElementById('map'), {
+  //   center: {lat: -34.397, lng: 150.644},
+  //   zoom: 8
+  // });
+//}
