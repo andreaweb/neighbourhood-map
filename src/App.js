986dc3dd0e42adb5702721ecad09939a2db84f4a
andreaweb <deiainluv@gmail.com>
2018-08-21 19:46:44 -0300

    first place in array gets info from foursquare

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 //import * as MapsAPI from './Map.js';
+import update from 'immutability-helper';
 import {Filter} from './Filter';
 import './App.css';
 //import google from "https://maps.googleapis.com/maps/api/js?key=AIzaSyAMSLE6fujNqNvj7opx7S3URDb9z_w_HyI&callback=initMap";
@@ -28,7 +29,8 @@ class GoogleMapsContainer extends React.Component {
         {
           title: 'Carioca Mall', 
           name: 'Carioca Mall', 
-          position: { lat: -22.8504633, lng: -43.3110845 }
+          position: { lat: -22.8504633, lng: -43.3110845 },
+          id: '4c49891420ab1b8dec01e516'
         },
         {
           title: 'VdC Bus Station', 
@@ -94,7 +96,17 @@ class GoogleMapsContainer extends React.Component {
     .then(
       data => {
         let mapData = data; console.log(mapData); 
-        this.setState({  })
+        this.setState({ 
+          "places": update(this.state.places, { 
+              [0]: { 
+                $set: {...this.state.places[0],
+                  'first tip' : data.response.venue.tips.groups[0].items[0].text,
+                  'Status': data.response.venue.popular.richStatus.text,
+                  'Phone Number': data.response.venue.contact.formattedPhone
+                } 
+              }
+            }) 
+        })
         console.log("First tip: "+data.response.venue.tips.groups[0].items[0].text); 
         console.log("Status: "+data.response.venue.popular.richStatus.text);
         console.log("Phone Number: "+data.response.venue.contact.formattedPhone); 
